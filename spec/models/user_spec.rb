d047398ea0782c6be02e7ec0649883fe53670866
Meredith Underell <m.underell@gmail.com>
2012-06-21 14:55:28 -0600

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -108,6 +108,12 @@ describe User do
 		end	
 	end
 
+	describe "email address with mixed case" do
+		@user.email = mixed_case_email
+		@user.save
+		@user.reload.email.should == mixed_case_email.downcase
+	end
+
 	end
 
 end
