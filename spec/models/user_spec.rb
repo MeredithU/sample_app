9f85276622f1339ab8d93a3214de33a9177afd34
Meredith Underell <m.underell@gmail.com>
2012-08-15 12:22:47 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,8 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) } 
 	it { should respond_to(:password_confirmation) }  
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -112,6 +114,11 @@ describe User do
 		@user.email = mixed_case_email
 		@user.save
 		@user.reload.email.should == mixed_case_email.downcase
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 
 	end
